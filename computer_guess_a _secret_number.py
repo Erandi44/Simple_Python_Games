6b7639eed59f03748d1865a2e4f56540d0c26133
Erandi44 <55404318+Erandi44@users.noreply.github.com>
2022-06-10 13:34:54 +0530

    Add files via upload

---
+++ computer_guess_a _secret_number.py
@@ -0,0 +1,29 @@
+# Computer guess secret number that we have chosen.
+import random
+
+    
+def computer_guess():
+    print("You can specify a range for the number you've picked.")
+    low = int(input("Lower end of the range: "))
+    high = int(input("Higher end of the range: "))
+    response = ""
+    
+    while response != "C":
+        guess = random.randint(low,high)
+        print(guess)
+        response = input("Is the guess too high(H), too low(L) or correct(C): ")
+        
+        if response =="L":
+            low = guess
+            continue
+        elif response == "H":
+            high = guess
+            continue
+        else:
+            print(f"Yay!The computer predicts your number {guess} correctly!")
+    
+def main():
+    computer_guess()
+    
+if __name__ == "__main__":
+    main()
